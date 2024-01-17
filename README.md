5b86dd3f15cb572dd282332c991aafcde7387989
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-01-17 15:22:18 +0700

    web

--- README.md
+++
@@ -1,2 +0,0 @@
-# comp1841-co1103
-Web Programming 1 - CO1103 - University of Greenwich (Vietnam)
---
+++ week1/demo.js
@@ -0,0 +1,19 @@
+function welcome() {
+   alert("Welcome to my website !");
+}
+
+function confirmDelete() {
+   confirm('Are you sure to delete this file ?');
+}
+
+function setBlueColor() {
+   document.getElementById('hanoi').style.color = "Blue";
+}
+
+function setGreenColor() {
+   document.getElementById('hanoi').style.color = "Green";
+}
+
+function changeText() {
+   document.getElementById('hanoi').innerHTML = "Greenwich Vietnam";
+}
