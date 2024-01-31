4c5cc613e75b285c80330ad445dda5f7977fec64
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-01-31 13:26:27 +0700

    lab3

---
+++ lab3/review.php
@@ -0,0 +1,31 @@
+<?php
+//"echo", "print" are used to display output
+echo "Web Programming 1 <br>";
+print "<h1 style='color: red'; >Greenwich Vietnam</h1>";
+
+//declare variable with "$" symbol
+$year = 2024;
+
+//dot symbol (.) is used for concatenation
+echo "We are living in " . $year;
+
+//PHP file extension: .php
+//PHP file can contain both PHP & HTML codes
+
+//2 main ways to run PHP codes
+
+/*
+1A. Install Xampp
+1B. Copy/Put code inside "C:/xampp/htdocs"
+1C. Start Xampp with Apache service
+1D. Open web browser and type address:
+    localhost/file_name
+Note: localhost === C:/xampp/htdocs
+*/
+
+/*
+2A. Install extension "PHP Server" on VS Code
+2B. Right click on selected file and select
+"PHP Server: Serve project"
+*/
+?>
